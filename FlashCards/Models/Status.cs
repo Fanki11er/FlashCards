aea7ba6cad5c5d753f55695ff13d1c1efaaf0671
Ewela <ewela28@poczta.onet.pl>
2022-04-16 11:54:48 +0200

    Added FlashCardsController

---
+++ FlashCards/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlashCards.Models
+{
+    public class Status
+    {
+        [Required]
+        public int AllAmount { get; set; }
+        [Required]
+        public int ToLearnAmount { get; set; }
+        [Required]
+        public int NewAmount { get; set; }
+    }
+}
