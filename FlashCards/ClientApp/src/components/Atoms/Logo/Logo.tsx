c15d20b952814e9de82d5e8fb72f4c89767e3774
Ewela <ewela28@poczta.onet.pl>
2022-04-25 21:10:42 +0200

    Added footer component

--- FlashCards/ClientApp/src/components/Atoms/Logo/Logo.tsx
+++ FlashCards/ClientApp/src/components/Atoms/Logo/Logo.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as Logo } from '../../../Assets/Svg/Logo.svg';
+import { ReactComponent as TransparentLogo } from '../../../Assets/Svg/TransparentLogo.svg';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
@@ -12,4 +13,9 @@ export default StyledLogo;
 export const Test = styled(Link)`
   width: fit-content;
   height: fit-content;
+`;
+
+export const StyledTransparentLogo = styled(TransparentLogo)`
+      width: 20%;
+      height: 80%;
 `;
