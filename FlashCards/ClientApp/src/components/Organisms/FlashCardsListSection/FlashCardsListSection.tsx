3d2ba76ca087bfed826406507597d909cc1f37c6
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2022-04-27 22:16:33 +0200

    Added filter input to flashcards list

--- FlashCards/ClientApp/src/components/Organisms/FlashCardsListSection/FlashCardsListSection.tsx
+++ FlashCards/ClientApp/src/components/Organisms/FlashCardsListSection/FlashCardsListSection.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import endpoints from '../../../Api/endpoints';
 import useAxiosPrivate from '../../../Hooks/useAxiosPrivate';
 import { FlashCard } from '../../../Interfaces/Interfaces';
+import { FilterInput } from '../../Atoms/FilterInput/FilterInput';
 import { FormError } from '../../Atoms/FormError/FormError';
-import { TextField } from '../../Atoms/TextField/TextField';
 import { StyledConnectionInfo } from '../EditFlashCardForm/EditFlashCardForm.styled';
 import FlashCardsList from '../FlashCardsList/FlashCardsList';
 import { FlashCardsListSectionWrapper } from './FlashCardsListSection.styles';
@@ -15,10 +15,28 @@ const FlashCardsListSection = (props: Props) => {
   const { openModal } = props;
   const { allFlashCards } = endpoints;
   const [flashCards, setFlashCards] = useState<FlashCard[]>([]);
+  const [filteredFlashCards, setFilteredFlashCards] = useState<FlashCard[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setError] = useState('');
   const axiosPrivate = useAxiosPrivate();
+  const [keyword, setKeyWord] = useState('');
 
+  const search = (e: ChangeEvent<HTMLInputElement>) => {
+    setTimeout(() => {
+      setKeyWord(e.target.value);
+    }, 500);
+  };
+
+  useEffect(() => {
+    const reg = keyword ? new RegExp(keyword.toLowerCase()) : '';
+    const filter = () => {
+      const results = flashCards.filter((flashCard: FlashCard) => {
+        return `${flashCard.frontText} ${flashCard.backText}`.toLowerCase().match(reg);
+      });
+      setFilteredFlashCards(results);
+    };
+    filter();
+  }, [keyword, flashCards]);
   useEffect(() => {
     let isMounted = true;
     const controller = new AbortController();
@@ -60,8 +78,8 @@ const FlashCardsListSection = (props: Props) => {
 
   return (
     <FlashCardsListSectionWrapper>
-      <TextField />
-      {isLoading ? <StyledConnectionInfo /> : <FlashCardsList flashCards={flashCards} openModal={openModal} />}
+      <FilterInput placeholder="Filter" type="search" onChange={(e) => search(e)} />
+      {isLoading ? <StyledConnectionInfo /> : <FlashCardsList flashCards={keyword ? filteredFlashCards : flashCards} openModal={openModal} />}
       {isError ? <FormError>{isError}</FormError> : null}
     </FlashCardsListSectionWrapper>
   );
